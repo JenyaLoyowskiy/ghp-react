108fd0cf4903b03dd6e96a4e24840ff397cba884
Jenya <loyowskiy@gmail.com>
2019-11-06 11:30:11 +0200

    Updates

---
+++ precache-manifest.c772ed486d9eb55fda47af544fedd58b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125d2dd8dbb2673475eaa9d86335a24d",
+    "url": "/ghp-react/index.html"
+  },
+  {
+    "revision": "fc65e005e0f212d92807",
+    "url": "/ghp-react/static/css/main.1f31f066.chunk.css"
+  },
+  {
+    "revision": "9478c8349e729fd9271d",
+    "url": "/ghp-react/static/js/2.6ed33c5d.chunk.js"
+  },
+  {
+    "revision": "fc65e005e0f212d92807",
+    "url": "/ghp-react/static/js/main.adafe701.chunk.js"
+  },
+  {
+    "revision": "e8e785438b0e8388aaf7",
+    "url": "/ghp-react/static/js/runtime-main.3dda95ab.js"
+  }
+]);
